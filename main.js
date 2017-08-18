eb0ed487898f8a1f28713ba06e0f95e48ff18bab
zhaiyuxiu <zhai-yx@ohtudou.com>
2017-08-18 17:05:34 +0800

    zhaiyuxiu new

--- main.js
+++ main.js
@@ -2,6 +2,24 @@
 
 // 引入hello模块:
 var greet = require('./hello');
+var path = require('path');
+var express = require('express');
+var app = express();
+
+//app.set('view engine', 'jade'); // 设置模板引擎
+//app.set('views', path.join(__dirname, 'template'));  // 设置模板相对路径(相对当前目录)
+
+app.get('/', function(request, response) {
+    var res = request.get("X-Requested-With");
+    response.render('jCanvasDemo/login.html');
+});
+
+var server = app.listen(8081, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("应用实例，访问地址为 http://%s:%s", host, port);
+});
 
 var s = 'Michael';
 
