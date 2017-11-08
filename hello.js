075fb0817ea6ad86cf7b4bd75e8168e230f39b37
zhaiyuxiu <zhai-yx@ohtudou.com>
2017-11-08 16:10:00 +0800

    zhaiyuxiu 执行系统命令

--- hello.js
+++ hello.js
@@ -1,42 +1,10 @@
 'use strict';
 
-function greet(name) {
-    console.log("基本数据类型------------------------------------");
-    var a = "a";
-    var b = a;
-    b = "c";
-    console.log("a   " + a);
-    console.log("b   " + b);
-
-
-    console.log("浅复制------------------------------------");
-    //复制一个对象
-    var a = {name: "aaa"};
-    var b = a;
-    console.log("a.name   " + a.name);
-    console.log("b.name   " + b.name);
-
-    //对b对象中的变量进行修改
-    b.name = "bbb";
-    console.log("a.name   " + a.name);
-    console.log("b.name   " + b.name);
-    //可以看到b对象改变了a对象中的值，这证明他们指向的是同一个内存空间。
-
-
-    console.log("深复制------------------------------------");
-    //那么怎么复制一个拥有自己独立空间的对象呢？
-    a.name = "aaa";
-    function deepCopy(a) {
-        var c = {};
-        for (var p in a) {
-            c[p] = a[p];
-        }
-        return c;
+var greet = {
+    hello:function() {
+        console.log('hello world');
     }
-    b = deepCopy(a);
-    b.name = "bbb";
-    console.log("a.name   " + a.name);
-    console.log("b.name   " + b.name);
-}
+
+};
 
 module.exports = greet;
--- main.js
+++ main.js
@@ -5,13 +5,32 @@ var greet = require('./hello');
 var path = require('path');
 var express = require('express');
 var app = express();
+var exec = require('child_process').exec;
+var cmdStr = 'ipconfig';
+
+
 
 //app.set('view engine', 'jade'); // 设置模板引擎
 //app.set('views', path.join(__dirname, 'template'));  // 设置模板相对路径(相对当前目录)
 
 app.get('/', function(request, response) {
     var res = request.get("X-Requested-With");
-    response.render('jCanvasDemo/login.html');
+    //response.render('template/index.jade');
+
+    exec(cmdStr, function(err,stdout,stderr){
+        if(err) {
+            console.log('get weather api error:'+stderr);
+        } else {
+            /*
+             这个stdout的内容就是上面我curl出来的这个东西：
+             {"weatherinfo":{"city":"北京","cityid":"101010100","temp":"3","WD":"西北风","WS":"3级","SD":"23%","WSE":"3","time":"21:20","isRadar":"1","Radar":"JC_RADAR_AZ9010_JB","njd":"暂无实况","qy":"1019"}}
+             */
+            //var data = JSON.parse(stdout);
+            //var str = iconv.decode(stdout, 'GBK');
+            //console.log("成功" +stdout);
+            response.send('<textarea>' + stdout + '</textarea>');
+        }
+    });
 });
 
 var server = app.listen(8081, function() {
@@ -23,4 +42,4 @@ var server = app.listen(8081, function() {
 
 var s = 'Michael';
 
-greet(s); // Hello, Michael!
+greet.hello(); // Hello, Michael!
